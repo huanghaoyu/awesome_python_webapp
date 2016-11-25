e2278e887ec7af617a58be20065f077180b67e81
huanghaoyu <hy.huang@foxmail.com>
2016-11-25 17:51:26 +0800

    Add User model.

--- blogs/models.py
+++ blogs/models.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import time
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    admin = models.BooleanField()
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='images/user_images/', max_length=204800)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class Blog(models.Model):
+    
